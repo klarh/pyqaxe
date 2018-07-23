5d822ac039e8de05e8166fcfd0d8d9ec7012d024
Matthew Spellings <mspells@umich.edu>
2018-07-23 18:55:02 -0400

    Add GTAR data source

--- pyqaxe/cache.py
+++ pyqaxe/cache.py
@@ -4,11 +4,24 @@ import pickle
 import sqlite3
 import json
 import re
+import weakref
+import hashlib
 
 class Cache:
+    opened_caches_ = weakref.WeakValueDictionary()
+
     def __init__(self, location=':memory:'):
         self.connection_ = sqlite3.connect(
             location, detect_types=sqlite3.PARSE_DECLTYPES)
+
+        unique_location = location
+        if location == ':memory:':
+            while unique_location in self.opened_caches_:
+                match = re.search(r'(\d+)$', unique_location)
+                index = 1 if match is none else int(match.groups()[0])
+                unique_location = ':memory:{}'.format(index + 1)
+        self.unique_id = hashlib.sha256(unique_location.encode('UTF-8')).hexdigest()
+        self.opened_caches_[self.unique_id] = self
 
         with self.connection_ as conn:
             conn.execute(
@@ -21,13 +34,15 @@ class Cache:
                 'CONSTRAINT unique_path '
                 'UNIQUE (path, data_source) ON CONFLICT IGNORE)')
 
-            data_sources = {}
+            self.data_sources = {}
             for (rowid, pickle_data) in conn.execute(
                     'SELECT rowid, pickle from data_sources'):
-                data_source = data_sources[rowid] = pickle.loads(pickle_data)
+                data_source = self.data_sources[rowid] = pickle.loads(pickle_data)
                 data_source.index(self, conn, rowid, force=False)
 
-        self.data_sources = data_sources
+    @classmethod
+    def get_opened_cache(cls, unique_id):
+        return cls.opened_caches_[unique_id]
 
     def index(self, data_source, force=False):
         with self.connection_ as conn:
@@ -43,15 +58,16 @@ class Cache:
 
             if force or stored_update_time is None:
                 begin_time = datetime.datetime.now()
-                data_source.index(self, cursor, rowid, force=force)
+                # force the first index if this source hasn't been indexed before
+                data_source.index(self, cursor, rowid, force=True)
                 cursor.execute('UPDATE data_sources SET update_time = ? WHERE rowid = ?',
                              (begin_time, rowid))
 
             self.data_sources[rowid] = data_source
 
-    def query(self, query):
+    def query(self, *args, **kwargs):
         with self.connection_ as conn:
-            return conn.execute(query)
+            return conn.execute(*args, **kwargs)
 
     def close(self):
         self.connection_.close()
---
+++ pyqaxe/data_sources/gtar.py
@@ -0,0 +1,76 @@
+import gtar
+import json
+import sqlite3
+from .. import Cache
+
+def encode_gtar_data(path, file_id, cache_id):
+    return json.dumps([path, file_id, cache_id]).encode('UTF-8')
+
+def convert_gtar_data(contents):
+    (path, file_id, cache_id) = json.loads(contents.decode('UTF-8'))
+    cache = Cache.get_opened_cache(cache_id)
+    for row in cache.query('SELECT * from files WHERE rowid = ?', (file_id,)):
+        # set row for open_file below
+        pass
+
+    with cache.open_file(row, 'rb') as f:
+        with gtar.GTAR(f.name, 'r') as traj:
+            return traj.readPath(path)
+
+class GTAR:
+    def __init__(self):
+        pass
+
+    def index(self, cache, conn, data_source=None, force=False):
+        self.check_adapters()
+
+        conn.execute('CREATE TABLE IF NOT EXISTS gtar_records '
+                     '(path TEXT, gtar_group TEXT, gtar_index TEXT, name TEXT, '
+                     'file_id INTEGER, cache_id TEXT, data GTAR_DATA, '
+                     'CONSTRAINT unique_gtar_path '
+                     'UNIQUE (path, file_id, cache_id) ON CONFLICT IGNORE)')
+
+        # don't do file IO if we aren't forced
+        if not force:
+            return
+
+        for row in conn.execute(
+                'SELECT rowid, * from files WHERE path LIKE "%.zip" OR '
+                'path LIKE "%.tar" OR path LIKE "%.sqlite"'):
+            file_id = row[0]
+            row = row[1:]
+            with cache.open_file(row, 'rb') as f:
+                with gtar.GTAR(f.name, 'r') as traj:
+                    for record in traj.getRecordTypes():
+                        group = record.getGroup()
+                        name = record.getName()
+                        for frame in traj.queryFrames(record):
+                            record.setIndex(frame)
+                            path = record.getPath()
+
+                            encoded_data = encode_gtar_data(
+                                path, file_id, cache.unique_id)
+                            values = (path, group, frame, name, file_id,
+                                      cache.unique_id, encoded_data)
+                            conn.execute(
+                                'INSERT INTO gtar_records VALUES (?, ?, ?, ?, ?, ?, ?)',
+                                values)
+        pass
+
+    @classmethod
+    def check_adapters(cls):
+        try:
+            if cls.has_registered_adapters:
+                return
+        except AttributeError:
+            # hasn't been registered yet, run the rest of this function
+            pass
+
+        sqlite3.register_converter('GTAR_DATA', convert_gtar_data)
+        cls.has_registered_adapters = True
+
+    def __getstate__(self):
+        return []
+
+    def __setstate__(self, state):
+        self.__init__(*state)
