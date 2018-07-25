480837ee415bf07f36c4b0e721be07aba9006e90
Matthew Spellings <mspells@umich.edu>
2018-07-25 11:46:47 -0400

    Add exclude_regexes to Directory

--- pyqaxe/mines/directory.py
+++ pyqaxe/mines/directory.py
@@ -1,4 +1,6 @@
 import os
+import re
+
 from .. import Cache
 
 class Directory:
@@ -8,11 +10,19 @@ class Directory:
     subdirectories of a given root directory.
 
     :param root: Base directory to begin searching
-    :param exclude_suffixes: Iterable of suffixes that should be excluded from addition to the list of files (i.e. ['txt', 'xml'])
+    :param exclude_regexes: Iterable of regex patterns that should be excluded from addition to the list of files upon a successful search
+    :param exclude_suffixes: Iterable of suffixes that should be excluded from addition to the list of files
+
+    Examples::
+
+        cache.index(Directory(exclude_regexes=[r'/\..*']))
+        cache.index(Directory(exclude_suffixes=['txt', 'zip']))
 
     """
-    def __init__(self, root=os.curdir, exclude_suffixes=()):
+    def __init__(self, root=os.curdir, exclude_regexes=(), exclude_suffixes=()):
         self.root = root
+        self.exclude_regexes = set(exclude_regexes)
+        self.compiled_regexes_ = [re.compile(pat) for pat in self.exclude_regexes]
         self.exclude_suffixes = set(exclude_suffixes)
         self.check_adapters()
 
@@ -34,16 +44,18 @@ class Directory:
             return
 
         for (dirpath, dirnames, fnames) in os.walk(self.root, followlinks=True):
-            # for dirname in dirnames:
-            #     target_path = os.path.join(dirpath, dirname, '')
-            #     cache.insert_file(conn, mine_id, target_path)
             for fname in fnames:
-                if not fname.split('.')[-1] in self.exclude_suffixes:
-                    target_path = os.path.join(dirpath, fname)
+                target_path = os.path.join(dirpath, fname)
+                valid = all([
+                    fname.split('.')[-1] not in self.exclude_suffixes,
+                    all(regex.search(target_path) is None for regex in self.compiled_regexes_)
+                    ])
+                if valid:
                     cache.insert_file(conn, mine_id, target_path)
 
     def __getstate__(self):
-        return [self.root, list(sorted(self.exclude_suffixes))]
+        return [self.root, list(sorted(self.exclude_regexes)),
+                list(sorted(self.exclude_suffixes))]
 
     def __setstate__(self, state):
         self.__init__(*state)
